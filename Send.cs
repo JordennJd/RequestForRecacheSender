1362f4dd77d37cce56ed355e65e65ac2678aca34
Danil <112797010+JordennJd@users.noreply.github.com>
2023-09-20 23:20:10 +0300

    update

--- Send.cs
+++ Send.cs
@@ -6,16 +6,21 @@ using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 {
 
-    channel.ExchangeDeclare(exchange: "SuaiScheduleApi", type: ExchangeType.Fanout);
-    string message = "Tvou doch ebut a ty chto?";
-    var body = Encoding.UTF8.GetBytes(message);
-    int i = 0;
-    channel.BasicPublish(exchange: "SuaiScheduleApi",
-        routingKey: "",
-        basicProperties: null,
-        body: body);
-    i++;
+    while (true)
+    {
+        Thread.Sleep(1800000);
+        channel.ExchangeDeclare(exchange: "SuaiScheduleApi", type: ExchangeType.Fanout);
+        string message = "update";
+        var body = Encoding.UTF8.GetBytes(message);
+        int i = 0;
+        channel.BasicPublish(exchange: "SuaiScheduleApi",
+            routingKey: "",
+            basicProperties: null,
+            body: body);
+        i++;
 
-    Console.WriteLine($" [x] Sent {message}");
-    Console.WriteLine(" Press [enter] to exit.");
+        Console.WriteLine($" [x] Sent {message}");
+        Console.WriteLine(" Press [enter] to exit.");
+        
+    }
 }
