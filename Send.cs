acda1ad706cc1f8bf8e90322417d44929a857114
Danil <112797010+JordennJd@users.noreply.github.com>
2023-09-21 11:38:45 +0300

    Добавил проверку на версию

--- Send.cs
+++ Send.cs
@@ -1,26 +1,49 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
 using RabbitMQ.Client;
 // Console.WriteLine(DateTime.Now.AddDays(1).Date.ToString("d"));
 var factory = new ConnectionFactory { HostName = "rabbitmq" };
+string lastDBVersion = "";
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 {
 
+
     while (true)
     {
+        string currentDBVersion = GetRequest("http://194.226.199.230:9002/api/db/version");
+        Console.WriteLine("Версия проверена");
         Thread.Sleep(1800000);
-        channel.ExchangeDeclare(exchange: "SuaiScheduleApi", type: ExchangeType.Fanout);
-        string message = "update";
-        var body = Encoding.UTF8.GetBytes(message);
-        int i = 0;
-        channel.BasicPublish(exchange: "SuaiScheduleApi",
-            routingKey: "",
-            basicProperties: null,
-            body: body);
-        i++;
+        if (currentDBVersion != lastDBVersion)
+        {
+            channel.ExchangeDeclare(exchange: "SuaiScheduleApi", type: ExchangeType.Fanout);
+            string message = "upda";
+            var body = Encoding.UTF8.GetBytes(message);
+            int i = 0;
+            channel.BasicPublish(exchange: "SuaiScheduleApi",
+                routingKey: "",
+                basicProperties: null,
+                body: body);
+            i++;
 
-        Console.WriteLine($" [x] Sent {message}");
-        Console.WriteLine(" Press [enter] to exit.");
-        
+            Console.WriteLine($" [x] Sent {message}");
+            Console.WriteLine(" Press [enter] to exit.");
+            
+        }
+
+        lastDBVersion = currentDBVersion;
+
     }
+}
+string GetRequest(string URL)
+{
+    WebRequest reqGET = WebRequest.Create(URL);
+    WebResponse resp = reqGET.GetResponse();
+    Stream stream = resp.GetResponseStream();
+    StreamReader sr = new StreamReader(stream);
+    string s = sr.ReadToEnd();
+    stream.Close();
+    sr.Close();
+    resp.Close();
+    return s;
 }
